86f949179c5ebf7a7842c76fd97b899687b22b3a
Shafkat Rahman <rahman.sakeeb@gmail.com>
2023-09-28 10:29:27 -0400

    Updated streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,16 +3,27 @@ from langchain.llms import OpenAI
 
 st.title('🦜🔗 Quickstart App')
 
+@st.cache(allow_output_mutation=True)
+def get_llm(api_key):
+    return OpenAI(temperature=0.7, openai_api_key=api_key)
+
+def generate_response(llm, input_text):
+    try:
+        response = llm(input_text)
+        st.info(response)
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
+
 openai_api_key = st.sidebar.text_input('OpenAI API Key')
 
-def generate_response(input_text):
-  llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
-  st.info(llm(input_text))
-
 with st.form('my_form'):
-  text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
-  submitted = st.form_submit_button('Submit')
-  if not openai_api_key.startswith('sk-'):
-    st.warning('Please enter your OpenAI API key!', icon='⚠')
-  if submitted and openai_api_key.startswith('sk-'):
-    generate_response(text)
+    text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
+    submitted = st.form_submit_button('Submit')
+
+    if not openai_api_key.startswith('sk-'):
+        st.warning('Please enter your OpenAI API key!', icon='⚠')
+
+    if submitted and openai_api_key.startswith('sk-'):
+        llm = get_llm(openai_api_key)
+        with st.spinner('Generating response...'):
+            generate_response(llm, text)
